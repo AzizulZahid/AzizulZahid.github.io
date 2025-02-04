84b8a41d7ec1939cd842f1f867bb8de095def9c3
AzizulZahid <130304118+AzizulZahid@users.noreply.github.com>
2025-02-04 06:05:02 +0000

    Deploying to gh-pages from @ AzizulZahid/AzizulZahid.github.io@e9890c916cec98e8e455c0c769a6a5c5c8a98424 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -106,6 +106,34 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%61%7A%61%68%69%64@%76%6F%6C%73.%75%74%6B.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/AzizulZahid", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/azizul-zahid-1b980b202", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=nTY1Y-AAAAAJ&hl=en", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
