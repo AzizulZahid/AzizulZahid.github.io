6919c47cb624b8bc9670d52e8d7707649e993206
AzizulZahid <130304118+AzizulZahid@users.noreply.github.com>
2025-02-09 20:54:54 +0000

    Deploying to gh-pages from @ AzizulZahid/AzizulZahid.github.io@1e68913067bf4cae4d08b0381abbb95bc6646563 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/_pages/cv/";
+            window.location.href = "/assets/pdf/CV_AzizulZahid.pdf";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
       
