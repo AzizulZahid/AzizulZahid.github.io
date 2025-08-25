6b24de2d5db8f846d0e8556086979bfa4796336e
AzizulZahid <130304118+AzizulZahid@users.noreply.github.com>
2025-08-25 15:42:00 +0000

    Deploying to gh-pages from @ AzizulZahid/AzizulZahid.github.io@da20907d7807ddf03261d013b1f4e335e1030857 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "A collections of my research world.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
